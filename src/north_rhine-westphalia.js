08c7e4ffd38ddb72d458471caba2cb0d063c646b
baslr <baslr@users.noreply.github.com>
2019-09-26 22:46:53 +0200

    added generator for dtm1 download files (north rhine-westphalia

---
+++ src/north_rhine-westphalia.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const fs = require('fs');
+
+const req = new (require('request-easy').https)({
+  hostname: 'www.opengeodata.nrw.de',
+  path: '/produkte/geobasis/dgm/dgm1/index.json'
+});
+
+req.onError = (con, cb, err) => console.log(err);
+
+async function run() {
+  const [status, headers, body] = await req.asyncGet();
+
+  if (200 !== status) {
+    console.log('cant fetch links. try again.');
+    return;
+  }
+
+  const links = JSON.parse(body).datasets[0].files
+    .map(f => `https://www.opengeodata.nrw.de/produkte/geobasis/dgm/dgm1/${encodeURIComponent(f.name)}`)
+    .join('\n');
+
+  fs.writeFileSync(`${__dirname}/../data/north_rhine-westphalia.txt`, links + '\n');
+}
+
+run();
