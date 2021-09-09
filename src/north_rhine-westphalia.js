0162c7a1a2b24473d38b4ad307188355b9d80d13
baslr <baslr@users.noreply.github.com>
2021-09-09 19:05:50 +0200

    update north rhine westphalia data

--- src/north_rhine-westphalia.js
+++ src/north_rhine-westphalia.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 const req = new (require('request-easy').https)({
   hostname: 'www.opengeodata.nrw.de',
-  path: '/produkte/geobasis/dgm/dgm1/index.json'
+  path: '/produkte/geobasis/hm/dgm1_xyz/dgm1_xyz/index.json'
 });
 
 req.onError = (con, cb, err) => console.log(err);
@@ -18,7 +18,7 @@ async function run() {
   }
 
   const links = JSON.parse(body).datasets[0].files
-    .map(f => `https://www.opengeodata.nrw.de/produkte/geobasis/dgm/dgm1/${encodeURIComponent(f.name)}`)
+    .map(f => `https://www.opengeodata.nrw.de/produkte/geobasis/hm/dgm1_xyz/dgm1_xyz/${encodeURIComponent(f.name)}`)
     .join('\n');
 
   fs.writeFileSync(`${__dirname}/../data/north_rhine-westphalia.txt`, links + '\n');
