da32a77a615ed7b91785935d7ef4a75863ee176b
baslr <baslr@users.noreply.github.com>
2021-09-09 19:38:19 +0200

    update saxony data

--- src/saxony.js
+++ src/saxony.js
@@ -1,44 +1,27 @@
+//@ts-check
 'use strict';
 
+const fs = require('fs');
+
 const req = new (require('request-easy').https)({
-    hostname: 'geocloud.landesvermessung.sachsen.de'
+    hostname: 'geoportal.sachsen.de',
+    path: '/cps/feed-viewer.html?dataset=true&url=https://geodownload.sachsen.de/inspire/el_atom/Dataset_el_dgm1.xml'
 });
 
-const fs = require('fs');
-let str = '';
 
-// build list with links
-const links = [];
-for (let x = 278; x <= 464; x += 2) {
-    for (let z = 5560; z <= 5728; z += 2) {
-        links.push([x, z]);
-    }
-}
+async function run() {
+    const [status, headers, body] = await req.asyncGet();
 
-
-async function testLink() {
-    const [[x, z]] = links.slice(0, 1);
-    const path = `/index.php/s/B1ioxntinuukMHd/download?path=%2F&amp;&files=dgm1_${x}${z}.zip`;
-    const [status, headers, body] = await req.asyncHead({ path });
-
-    if (200 === status) {
-        str += `https://geocloud.landesvermessung.sachsen.de${path}\n`;
-    }
-    else if (404 !== status) {
-        console.log('error get ', path);
-        console.log('status is', status);
-        console.log('retry');
-
+    if (200 !== status) {
+        console.log('cant fetch links. try again.');
         return;
     }
 
-    links.shift();
-}
+    const links = Array.from(
+        new Set(body.toString().match(/\"https.*dgm1_\d+\.zip\"/g)
+            .map(v => v.slice(1, -1)))
+    ).join('\n');
 
-async function run() {
-    while (links.length) {
-        await testLink();
-    }
-    fs.writeFileSync(`${__dirname}/../data/saxony.txt`, str);
+    fs.writeFileSync(`${__dirname}/../data/saxony.txt`, links + '\n');
 }
 run();
